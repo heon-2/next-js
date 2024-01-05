9ac768596e6df890513f60f0fcc288144a050930
이지헌 <jiheon_114@naver.com>
2024-01-05 19:05:22 +0900

    feat:강의 수강을 위한 project 생성 및 mongoDB 계정 연결

---
+++ forum-practice/app/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { MongoClient } from 'mongodb'
+import { connectDB } from '@/util/database.js'
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("codingapple")
+  // 컬렉션의 모든 documnet 꺼내오려면,
+  let result = await db.collection('post').find().toArray();
+  console.log(result)
+  return (
+    <div>
+      안녕
+    </div>
+  )
+}
