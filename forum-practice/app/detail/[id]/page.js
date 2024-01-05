745368fe11f86fe0d68548b0d6da65bfaea6be98
이지헌 <jiheon_114@naver.com>
2024-01-05 19:31:31 +0900

    feat: 게시물 id 별 상세페이지 기능 구현

--- forum-practice/app/detail/[id]/page.js
+++ forum-practice/app/detail/[id]/page.js
@@ -1,10 +1,11 @@
 import { ObjectId } from "mongodb";
 import { connectDB } from "@/util/database.js"
 
-export default async function Detail() {
+export default async function Detail(props) {
   let db = (await connectDB).db('codingapple')
   let result = await db.collection('post').findOne({_id : new ObjectId('6596d5f225d20b6d454a88f2')});
   console.log(result)
+  console.log(props.params.id) // 유저가 입력한 것은 props로 찍힘.
 
   return (
     <div>
